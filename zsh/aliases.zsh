b133007ddca38e2dbb9dc3a3e774e34842192748
Jason Trill <jason@jasontrill.com>
2014-03-21 17:56:24 -0700

    Ackrc, hgrc, other changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,15 @@
-alias reload!='. ~/.zshrc'
-alias hack!='history | ack'
-alias nis!='npm install --save'
-alias nisd!='npm install --save-dev'
+alias reload='. ~/.zshrc'
+alias hack='history | ack'
+alias nis='npm install --save'
+alias nisd='npm install --save-dev'
+alias ns='npm search'
+
+alias bis='bower install --save'
+alias bs='bower search'
+
+# because typing 'cd' is A LOT of work!!
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
 
