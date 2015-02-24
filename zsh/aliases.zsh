c2f7b4d66c4973b6278f629c1d5a38a839a6c6e7
Jason Trill <jason@jasontrill.com>
2015-02-23 21:35:07 -0800

    MetaLab MBP1 2015-02-23

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,9 +7,22 @@ alias ns='npm search'
 alias bis='bower install --save'
 alias bs='bower search'
 
+alias vag='vagrant'
+
+alias npme='PATH=$(npm bin):$PATH'
+
+alias color='osascript ~/.color_meter.scpt'
+
 # because typing 'cd' is A LOT of work!!
 alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
+alias ......='cd ../../../../../'
+alias .......='cd ../../../../../../'
+alias ........='cd ../../../../../../../'
+alias .........='cd ../../../../../../../../'
+alias ..........='cd ../../../../../../../../../'
+alias ...........='cd ../../../../../../../../../../'
+
 
